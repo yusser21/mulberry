97b555e815cd8d32466880cbd5d369e3ff22de96
yusser21 <yusseralsuhail4@gmail.com>
2023-09-12 14:44:37 +0300

    .

---
+++ mullberry_app/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => (
+  <div className='bg-customBlue min-h-screen'>
+    {/* Navbar */}
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-xl font-semibold">
+          <a href="#">Your Logo</a>
+        </div>
+        <div className="hidden md:flex space-x-4">
+          <a href="#" className="text-white hover:text-gray-300">Home</a>
+          <a href="#" className="text-white hover:text-gray-300">About</a>
+          <a href="#" className="text-white hover:text-gray-300">Services</a>
+          <a href="#" className="text-white hover:text-gray-300">Contact</a>
+        </div>
+      </div>
+    </nav>
+
+    {/* Your page content goes here */}
+    <div className="container mx-auto mt-8 p-4">
+      {/* Add your content here */}
+    </div>
+  </div>
+);
+
+export default App;
+
